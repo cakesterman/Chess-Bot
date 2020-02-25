fd0053b9bbf5eee2bdebaf176bdd60fac787485c
Tyler Bull <tbtylerbull@gmail.com>
2020-02-25 09:19:10 -0500

    Rules works with Bishops now

--- chess_rules.py
+++ chess_rules.py
@@ -117,6 +117,18 @@ def check_valid_move(game_piece, current_pos, pos_to_move, chess_board):
 
             return False
 
+    elif game_piece == "Black Bishop" or game_piece == "White Bishop":
+
+        if pos_to_move in bishop_calculate_all_possible_moves(current_pos, chess_board):
+
+            return True
+
+        else:
+
+            return False
+
+        #print(bishop_calculate_all_possible_moves(current_pos, chess_board))
+
     # Temporary else for testing and moving all other game pieces
     else:
 
@@ -170,6 +182,147 @@ def knight_calculate_all_possible_moves(current_pos, chess_board):
             elif x == (current_pos[0] + 2) and y == (current_pos[1] - 1):
 
                 check_bounds_and_chess_board(x, y)
+
+    return all_possible_moves
+
+
+def bishop_calculate_all_possible_moves(current_pos, chess_board):
+
+    all_possible_moves = []
+
+    def search_left_down_diagonally():
+
+        x = current_pos[0]
+        y = current_pos[1]
+
+        possible = True
+        while possible:
+
+            x -= 1
+            y += 1
+
+            if chess_board.get((x, y)) is None:
+
+                if x == 0 or y == 7:
+                    possible = False
+
+                #print("At location ({}, {})".format(x, y))
+
+                all_possible_moves.append((x, y))
+
+            else:
+
+                possible = False
+
+    def search_right_down_diagonally():
+
+        x = current_pos[0]
+        y = current_pos[1]
+
+        possible = True
+        while possible:
+
+            x += 1
+            y += 1
+
+            if chess_board.get((x, y)) is None:
+
+                if x == 7 or y == 7:
+                    possible = False
+
+                #print("At location ({}, {})".format(x, y))
+
+                all_possible_moves.append((x, y))
+
+            else:
+
+                possible = False
+
+    def search_left_up_diagonally():
+
+        x = current_pos[0]
+        y = current_pos[1]
+
+        possible = True
+        while possible:
+
+            x -= 1
+            y -= 1
+
+            if chess_board.get((x, y)) is None:
+
+                if x == 0 or y == 0:
+                    possible = False
+
+                #print("At location ({}, {})".format(x, y))
+
+                all_possible_moves.append((x, y))
+
+            else:
+
+                possible = False
+
+    def search_right_up_diagonally():
+
+        x = current_pos[0]
+        y = current_pos[1]
+
+        possible = True
+        while possible:
+
+            x += 1
+            y -= 1
+
+            if chess_board.get((x, y)) is None:
+
+                if x == 7 or y == 0:
+                    possible = False
+
+                #print("At location ({}, {})".format(x, y))
+
+                all_possible_moves.append((x, y))
+
+            else:
+
+                possible = False
+
+    if current_pos[1] == 0:
+
+        if current_pos[0] > 0:
+
+            # Search left down diagonally
+            search_left_down_diagonally()
+
+        if current_pos[0] < 7:
+
+            # Search right down diagonally
+            search_right_down_diagonally()
+
+    else:
+
+        if current_pos[1] == 7 and 0 < current_pos[0] < 7:
+
+            search_left_up_diagonally()
+            search_right_up_diagonally()
+
+        else:
+
+            if current_pos[0] > 0:
+
+                search_left_down_diagonally()
+                search_right_down_diagonally()
+                search_left_up_diagonally()
+                search_right_up_diagonally()
+
+        if current_pos[0] == 0 and current_pos[1] < 7:
+
+            search_right_up_diagonally()
+            search_right_down_diagonally()
+
+        if current_pos[0] == 7 and current_pos[1] < 7:
+
+            search_left_up_diagonally()
+            search_left_down_diagonally()
 
     return all_possible_moves
 
